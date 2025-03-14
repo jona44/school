bde751ecd98108d3716a0910cd806102e009c35e
jona44 <tjmanyadza@live.com>
2025-03-14 22:30:12 +0200

    modified:   customadmin/templatetags/custom_tags.py

--- customadmin/templatetags/custom_tags.py
+++ customadmin/templatetags/custom_tags.py
@@ -2,7 +2,6 @@ from django import template
 from django.urls import reverse
 
 register = template.Library()
-
 @register.simple_tag
 def get_dashboard_url(user):
     if user.groups.filter(name='student').exists():
@@ -18,9 +17,22 @@ def get_dashboard_url(user):
     else:
         return reverse('default_dashboard')
     
+    
+   
 @register.filter(name='add_class')
 def add_class(field, css_class):
     return field.as_widget(attrs={"class": css_class})    
 
 
+
+@register.filter
+def get_item(queryset, key):
+    """
+    Gets the first item from a queryset where the id matches the key.
+    """
+    try:
+        return queryset.get(id=key)  # Use filter and take the first element
+    except Exception as e:
+        # Handle the case where no such item exists or MultipleObjectsReturned
+        return None
 
